5749c52c7439a1c8b8ffd07d83bfba69af898d9d
soccert <talyagriffin2004@gmail.com>
2018-07-23 20:02:42 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,3 +1,6 @@
 
 # Code your methods below
-def Lower
+def Lower Body
+  puts plant legs far apart, bend knees slightly and keep posture options_from_collection_for_select
+puts lift right foot
+return right foot  to the ground
