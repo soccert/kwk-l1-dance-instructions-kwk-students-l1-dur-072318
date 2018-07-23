b35d38448a2f99382034ad23ae228e98360f388f
soccert <talyagriffin2004@gmail.com>
2018-07-23 20:14:11 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -9,5 +9,10 @@ end
 def skip-step
   puts "skip-step left"
   puts "lower left foot to the ground"
-  puts "finishing with a small skip-step backward"
-  
+  puts "bounce right foot back up slightly, kicking it a few inches back"
+  puts "left,right,left and left"
+  puts "skip-step right"
+  puts " lower right to the ground"
+  puts "bounce right foot back up slightly, kicking it a few inches back"
+  puts " right,left,right and right"
+end
