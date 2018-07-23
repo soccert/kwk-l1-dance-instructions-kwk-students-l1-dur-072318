0700f919855b4a9f39847b7e4abf7667eb305ee2
soccert <talyagriffin2004@gmail.com>
2018-07-23 20:29:25 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,27 @@
 
 # Code your methods below
+def Lower Body
+  puts "plant legs far apart, bend knees slightly and keep posture"
+puts "lift right foot"
+puts " return right foot  to the ground"
+puts "finishing  with a small skip-step backward"
+end
+def skip-step
+  puts "skip-step left"
+  puts "lower left foot to the ground"
+  puts "bounce right foot back up slightly, kicking it a few inches back"
+  puts "left,right,left and left"
+  puts "skip-step right"
+  puts " lower right to the ground"
+  puts "bounce right foot back up slightly, kicking it a few inches back"
+  puts " right,left,right and right"
+end
+def upper Body
+  puts "holding your arms out in front of you, straight and chest level"
+  puts " cross your right wrist over your left and hold them together"
+  puts " lift your armm up and down in a loose bouncing movement, in time with the beat of the song.this movement is repeated eight times"
+end
+def lasso
+  puts " start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
+  puts " lift right arm up and point right elbow diagonally to the right"
+  
