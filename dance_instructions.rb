7e96f8a741707beed924b9ae975969c1d5fd01d6
soccert <talyagriffin2004@gmail.com>
2018-07-23 20:10:01 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,6 +1,13 @@
 
 # Code your methods below
 def Lower Body
-  puts plant legs far apart, bend knees slightly and keep posture options_from_collection_for_select
-puts lift right foot
-return right foot  to the ground
+  puts "plant legs far apart, bend knees slightly and keep posture"
+puts "lift right foot"
+puts " return right foot  to the ground"
+puts "finishing  with a small skip-step backward"
+end
+def skip-step
+  puts "skip-step left"
+  puts "lower left foot to the ground"
+  puts "finishing with a small skip-step backward"
+  
